30071ab37b183b5c2c9ba421a51a2896598c2712
Point-bo-f <bo.furustam@stud.pointcollege.fi>
2019-03-09 18:39:31 +0200

    Add files via upload

---
+++ Create dbo.Timesheet.sql
@@ -0,0 +1,27 @@
+﻿USE [TimesheetMobile]
+GO
+
+/****** Object: Table [dbo].[Timesheet] Script Date: 24.2.2019 13.08.54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Timesheet] (
+    [Id_Timesheet]      INT     IDENTITY (1, 1)        NOT NULL,
+    [Id_Customer]       INT             NULL,
+    [Id_Contractor]     INT             NULL,
+    [Id_Employee]       INT             NULL,
+    [Id_WorkAssignment] INT             NULL,
+    [StartTime]         DATETIME        NULL,
+    [StopTime]          DATETIME        NULL,
+    [Comments]          NVARCHAR (2000) NULL,
+    [WorkComplete]      BIT             NULL,
+    [CreatedAt]         DATETIME        NULL,
+    [LastModifiedAt]    DATETIME        NULL,
+    [DeletedAt ]        DATETIME        NULL,
+    [Active]            BIT             NULL
+);
+
+
